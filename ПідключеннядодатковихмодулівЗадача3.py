466b6c87c0919de64084c90529896ab6d3f974e2
alex <lazarch@users.noreply.github.com>
2021-02-23 12:11:58 +0200

    Add files via upload

---
+++ ПідключеннядодатковихмодулівЗадача3.py
@@ -0,0 +1,7 @@
+c='Програма для визначення площі круга'
+d=c.center(70)
+print(d)
+import math
+d=float(input('Введіть діаметр круга = '))
+s=math.pi*math.pow(d,2)/2
+print('Площа круга діаметром ',d,' дорівнює ',s)
